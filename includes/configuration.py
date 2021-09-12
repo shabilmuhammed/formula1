549669dcf7069db2d9ce4e4a54ee5cbd27dc0501
shabilmuhammed <47236472+shabilmuhammed@users.noreply.github.com>
2021-09-13 00:08:06 +0530

    Add files via upload

---
+++ includes/configuration.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1dl007/raw"
+processed_folder_path = "/mnt/formula1dl007/processed"
+presentation_folder_path = "/mnt/formula1dl007/presentation"
+
+# COMMAND ----------
+
